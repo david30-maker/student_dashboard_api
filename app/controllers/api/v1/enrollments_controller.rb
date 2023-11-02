8874745f2f9390818e685c18a01b34122faeb447
david30-maker <davidokpah396@gmail.com>
2023-11-02 17:51:22 +0100

    update functionalities to handle api

--- app/controllers/api/v1/enrollments_controller.rb
+++ app/controllers/api/v1/enrollments_controller.rb
@@ -1,18 +1,6 @@
 class Api::V1::EnrollmentsController < ApplicationController
     before_action :authenticate_user!
     skip_before_action :authenticate_user!, only: %i[show index]
-    
-    # GET /api/v1/enrollments
-    # def index
-    #     @enrollments = Enrollment.all
-    #     render json: @enrollments, status: :ok
-    # end
-    
-    # # GET /api/v1/enrollments/:id
-    # def show
-    #     @enrollment = Enrollment.find(params[:id])
-    #     render json: @enrollment, status: :ok
-    # end
 
     # POST /api/v1/enrollments
     def create
